0d5156698f48e985c31de1b0fd91e53025ab3a0f
chichieinstein <chichieinstein@gmail.com>
2023-12-23 07:54:59 +0000

    Generic GPU arrays in Rust

--- device-sys/src/lib.rs
+++ device-sys/src/lib.rs
@@ -1,6 +1,6 @@
 use std::ffi::c_void;
 extern "C" {
-    pub fn memory_allocate_device(inp : i32) -> *mut c_void;
+    pub fn memory_allocate_device(inp: i32) -> *mut c_void;
     pub fn memory_deallocate_device(inp: *mut c_void);
     pub fn transfer(dst: *mut c_void, src: *mut c_void, count: i32);
 }
--- rustdevice/src/lib.rs
+++ rustdevice/src/lib.rs
@@ -1,21 +1,20 @@
 extern crate device_sys;
 use device_sys::{memory_allocate_device, memory_deallocate_device, transfer};
+use num_complex::Complex;
 use std::ffi::c_void;
 use std::mem::size_of;
-use std::{default::Default, clone::Clone, fmt::{Display}};
-use num_complex::Complex;
+use std::{clone::Clone, default::Default, fmt::Display};
 
-
-pub struct DevicePtr<T: Display+Default+Clone> {
+pub struct DevicePtr<T: Display + Default + Clone> {
     ptr: *mut T,
     size: i32,
 }
 
-impl<T: Display+Default+Clone> DevicePtr<T> {
+impl<T: Display + Default + Clone> DevicePtr<T> {
     pub fn new(sz: i32) -> Self {
         // println!("Memory getting allocated in Rust\n");
         Self {
-            ptr: unsafe { memory_allocate_device(sz*(size_of::<T>() as i32)) } as *mut T,
+            ptr: unsafe { memory_allocate_device(sz * (size_of::<T>() as i32)) } as *mut T,
             size: sz,
         }
     }
@@ -24,24 +23,25 @@ impl<T: Display+Default+Clone> DevicePtr<T> {
         let right_num = {
             if count < self.size {
                 count
-            }
-            else {
+            } else {
                 self.size
             }
         };
         unsafe {
             let mut z = vec![T::default(); right_num as usize];
-            transfer(z.as_mut_ptr() as *mut c_void, self.ptr as *mut c_void, right_num);
+            transfer(
+                z.as_mut_ptr() as *mut c_void,
+                self.ptr as *mut c_void,
+                right_num,
+            );
             for ind in 0..(right_num as usize) {
-                println!(
-                    "{}\n", z[ind]
-                );
+                println!("{}\n", z[ind]);
             }
         };
     }
 }
 
-impl<T: Display+Default+Clone> Drop for DevicePtr<T> {
+impl<T: Display + Default + Clone> Drop for DevicePtr<T> {
     fn drop(&mut self) {
         // println!("Memory getting deallocated in Rust");
         unsafe { memory_deallocate_device(self.ptr as *mut c_void) };
